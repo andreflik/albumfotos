7e1954c5c63c7e6b540c2d2b1b5c137bfe408899
Andre Fagundes <andreflik@gmail.com>
2021-03-25 21:32:14 -0300

    Album de fotos Laravel + Angular V1

---
+++ app/Http/Controllers/PostControlador.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Post;
+
+class PostControlador extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+
+        $path = $request->file('arquivo')->store('imagens', 'public');
+
+        $post->nome = $request->nome;
+        $post->email = $request->email;
+        $post->titulo = $request->titulo;
+        $post->subtitulo = $request->subtitulo;
+        $post->mensagem = $request->mensagem;
+        $post->arquivo = $path;
+        $post->likes = 0;
+
+        $post->save();
+        return response($post, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        if (isset($post)) {
+            Storage::disk('public')->delete($post->arquivo);
+            $post->delete();
+            return 204;
+        }
+        return response ('Post não encontrado', 404);
+    }
+
+    public function like($id)
+    {
+        $post = Post::find($id);
+        if (isset($post)) {
+            $post->likes++;
+            $post->save();
+            return $post;
+        }
+        return response ('ID não encontrado', 404);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/', 'PostControlador@index');
+
+Route::post('/', 'PostControlador@store');
+
+Route::delete('/{id}', 'PostControlador@destroy');
+
+Route::get('/like/{id}', 'PostControlador@like');
+
+
+
